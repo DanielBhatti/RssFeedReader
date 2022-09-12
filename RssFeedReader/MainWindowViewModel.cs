f0b24abd6cbb3cd13f9d98618cdc5cb0986113b7
Daniel Bhatti <danielbbhatti@gmail.com>
2022-09-12 18:00:02 -0400

    Made some UI changes.  Trying to add filtering for the feed articles.  Progressing slowly but surely.

--- RssFeedReader/MainWindowViewModel.cs
+++ RssFeedReader/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
 using CodeHollow.FeedReader;
 using ReactiveUI;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace RssFeedReader
@@ -12,7 +14,9 @@ namespace RssFeedReader
         private string AppDataPath { get; }
         private string FeedListPath { get; }
 
-        public ObservableCollection<RssFeed> FeedCollection { get; }
+        public ObservableCollection<RssFeed> FullFeedCollection { get; }
+
+        public ObservableCollection<RssFeed> DisplayedFeedCollection { get => GetFilteredFeedCollection(); }
 
         private string _newUriFeedName;
         public string NewUriFeedName
@@ -28,6 +32,24 @@ namespace RssFeedReader
             set => this.RaiseAndSetIfChanged(ref _displayText, value);
         }
 
+        private string _filterText;
+        public string FilterText
+        {
+            get => _filterText;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _filterText, value);
+                this.RaisePropertyChanged(nameof(DisplayedFeedCollection));
+            }
+        }
+
+        private Article _selectedArticle;
+        public Article SelectedArticle
+        {
+            get => _selectedArticle;
+            set => this.RaiseAndSetIfChanged(ref _selectedArticle, value);
+        }
+
         public MainWindowViewModel()
         {
             AppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), nameof(RssFeedReader));
@@ -41,22 +63,30 @@ namespace RssFeedReader
             {
                 json = reader.ReadToEnd();
             }
-            if (!String.IsNullOrEmpty(json)) FeedCollection = JsonSerializer.Deserialize<ObservableCollection<RssFeed>>(json) ?? new();
-            else FeedCollection = new();
+            if (!String.IsNullOrEmpty(json)) FullFeedCollection = JsonSerializer.Deserialize<ObservableCollection<RssFeed>>(json) ?? new();
+            else FullFeedCollection = new();
         }
 
-        public async void AddFeed(string feedUri)
+        public async void AddFeed(string feedUriCsv)
         {
-            try
+            foreach (string feedUri in feedUriCsv.Split(',').Select(f => f.Trim()))
             {
-                Feed feed = await FeedReader.ReadAsync(feedUri);
-                FeedCollection.Add(RssFeed.FromCodeHollowFeedReader(feed));
-                SaveFeedList();
-                DisplayText = $"Successfully added feed {feed.Title} to list.";
-            }
-            catch (Exception e)
-            {
-                DisplayText = $"Failed to add feed with URI {feedUri} to list.\n\n{e}";
+                try
+                {
+                    Feed feed = await FeedReader.ReadAsync(feedUri);
+                    if (FullFeedCollection.Select(f => f.Uri).Contains(feedUri))
+                    {
+                        DisplayText = $"Feed with URI {feedUri} already exists as {feed.Title}.";
+                        return;
+                    }
+                    FullFeedCollection.Add(RssFeed.FromCodeHollowFeedReader(feed));
+                    SaveFeedList();
+                    DisplayText = $"Successfully added feed {feed.Title} to list.";
+                }
+                catch (Exception e)
+                {
+                    DisplayText = $"Failed to add feed with URI {feedUri} to list.\n\n{e}";
+                }
             }
         }
 
@@ -69,9 +99,27 @@ namespace RssFeedReader
         {
             using (StreamWriter writer = new StreamWriter(FeedListPath))
             {
-                string json = JsonSerializer.Serialize(FeedCollection);
+                string json = JsonSerializer.Serialize(FullFeedCollection);
                 writer.Write(json);
             }
+        }
+
+        public ObservableCollection<RssFeed> GetFilteredFeedCollection()
+        {
+            if (String.IsNullOrEmpty(FilterText)) return FullFeedCollection;
+
+            //todo add regex mode
+            Dictionary<RssFeed, int> feedWordFrequency = new();
+            foreach (RssFeed feed in FullFeedCollection)
+            {
+                feedWordFrequency[feed] = 0;
+                foreach (string filterWord in FilterText.Split(' '))
+                {
+                    string trimmedFilterWord = filterWord.Trim();
+                    if (feed.Title.Contains(trimmedFilterWord)) feedWordFrequency[feed]++;
+                }
+            }
+            return new ObservableCollection<RssFeed>(FullFeedCollection.Where(f => feedWordFrequency[f] > 0).OrderByDescending(f => feedWordFrequency[f]));
         }
     }
 }
